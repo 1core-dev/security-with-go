961fa011a359841a12661f8e53eeb1b3ae42f428
1CORE <1core@protonmail.com>
2025-05-20 15:46:15 +0300

    SSH client with password auth

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/1core-dev/security-with-go
 
 go 1.23.6
 
-require github.com/google/gopacket v1.1.19
+require (
+	github.com/google/gopacket v1.1.19
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
+)
 
 require (
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
---
+++ secure-shell/ssh-client/password_authenticating.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"golang.org/x/crypto/ssh"
+)
+
+var username = "username"
+var password = "password"
+var host = "example.com:22"
+
+func main() {
+	config := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh.Dial("tcp", host, config)
+	if err != nil {
+		log.Fatal("Error dialing server. ", err)
+	}
+
+	log.Println(string(client.ClientVersion()))
+}
